b1fbb676f29089a97f61d8ce53bd9aa1b344b47e
Shane W. Myers <myerss12@ferris.edu>
2015-03-26 20:41:02 -0400

    Text adventure to teach areonautics

---
+++ aeronautics/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aeronautics
+{
+    class Player
+    {
+        // TO ADD: Tree Path hashtable
+        // TO ADD: Inventory array
+    }
+}
