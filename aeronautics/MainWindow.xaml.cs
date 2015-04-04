2a4aabef2ab2d521876252045ff3b2bce6a477cf
Shane W. Myers <myerss12@ferris.edu>
2015-04-03 20:19:28 -0400

    Hash Work

--- aeronautics/MainWindow.xaml.cs
+++ aeronautics/MainWindow.xaml.cs
@@ -11,28 +11,82 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace aeronautics
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        //StreamReader
+        FileStream fs = null;
+        Player player;
+
+
         public MainWindow()
         {
+            
             InitializeComponent();
 
-            Player player = new Player();
+            player = new Player();
 
-            Debug.Text = "Starting from Here";
+            //Debug.Text = "Starting from Here";
+
+
+            // should try catch
+            // stream reader
+            // final stream
+
+            
+            
+                Debug.Text = "Starting from Here";
+            
+            
+
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            Debug.Text = "choice Clicked";
-            sender.ToString();
+            
+
+            Button b = (Button)(sender);
+
+            player.HashTableAdd(player.HashTableGetCount(), (string)b.Tag);
+
+
+            string s = "";
+
+            // prints out the stored text from the play's hashTable
+            for (int i = 0; i < player.HashTableGetCount(); i++)
+            {
+                s += (player.HashTableGetText(i) + ", ");
+            }
+
+
+            Debug.Text = s;
+
+
+            //Debug.Text = player.HashTableGetCount().ToString();   // able to add to the player's hashtable correctly
+            //Debug.Text = (string)b.Tag;                           // able to get the sender's tage successfully
+            //Debug.Text = "choice Clicked";                        // able to successfully click all buttons with the same function running each time
         }
 
+        private void Debug_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Choice Clicked");
+        }
+
+        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // verifies that the image can be clicked and trigger a function
+            //image1.Visibility = Visibility.Hidden;
+
+
+            image1.Source = new BitmapImage(new Uri(@"pack://application:,,,/aeronautics;component/Images/Chrysanthemum.jpg"));
+            Debug.Text = "Image Clicked.";
+        }
     }
 }
--- aeronautics/Player.cs
+++ aeronautics/Player.cs
@@ -2,12 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace aeronautics
 {
     class Player
     {
-        // TO ADD: Tree Path hashtable
-        // TO ADD: Inventory array
+        // Initilization Section
+        Hashtable PlayerHashtable = new Hashtable();
+        List<InventoryObject> PlayerInventory = new List<InventoryObject>();
+
+
+
+
+        /// <summary>
+        /// HashTable Methods
+        /// </summary>
+        /// <param name="x"></param>
+        public void HashTableAdd(int index, string text)
+        {
+            PlayerHashtable.Add(index, text);
+        }
+        public int HashTableGetCount()
+        {
+            return PlayerHashtable.Count;
+        }
+        public string HashTableGetText(int index)
+        {
+            return PlayerHashtable[index].ToString();
+        }
+
+
+
+
+        /// <summary>
+        /// Inventory Methods
+        /// </summary>
+        /// <param name="x"></param>
+        public void newInventoryObject(InventoryObject x)
+        {
+            PlayerInventory.Add(x);
+        }
+        public void removeInventoryObject(InventoryObject x)
+        {
+            PlayerInventory.Remove(x);
+        }
+        public void checkInventoryObject(int index)
+        {
+            PlayerInventory[index].ToString();
+        }
+
+        // TO ADD: Tree Path hashtable checks
+    }
+
+    class InventoryObject
+    {
+        string PicturePath = null;
+        int ObjectID;
+
+        public void SetPicturePath(string s)
+        {
+            PicturePath = s;
+        }
+
+        public void SetObjectID(int x)
+        {
+            ObjectID = x;
+        }
     }
 }
